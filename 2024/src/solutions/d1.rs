900aa6807dd9f5e23aec720a502ec03a4a71826b
Miquel de Domingo <miquelddg@gmail.com>
2024-12-03 11:39:29 +0100

    feat(2024): 2.b solution

--- 2024/src/solutions/d1.rs
+++ 2024/src/solutions/d1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::solutions::helpers::read_lines;
 
 pub fn solve_a() {
@@ -22,4 +24,23 @@ pub fn solve_a() {
         sum += i32::abs(left_val - right_val);
     });
     println!("{}", sum);
+}
+
+pub fn solve_b() {
+    let (positions, group): (Vec<i32>, HashMap<i32, i32>) = read_lines("d1.txt").iter().fold(
+        (Vec::new(), HashMap::new()),
+        |(mut positions, mut group), line| {
+            let values = line.split("   ").collect::<Vec<&str>>();
+            positions.push(values[0].parse::<i32>().unwrap());
+            *group.entry(values[1].parse::<i32>().unwrap()).or_insert(0) += 1;
+            (positions, group)
+        },
+    );
+
+    println!(
+        "{}",
+        positions
+            .iter()
+            .fold(0, |acc, x| { acc + group.get(x).unwrap_or(&0) * x })
+    );
 }
