ffb5b192bd2939376267ccd3beddba58295c2725
Miquel de Domingo <miquelddg@gmail.com>
2024-12-03 15:49:04 +0100

    chore: add data script and create monorepo

---
+++ scripts/src/main.rs
@@ -0,0 +1,50 @@
+use std::fs::write;
+use std::path::Path;
+
+use chrono::{Datelike, Utc};
+use clap::Parser;
+use reqwest::blocking::Client;
+use reqwest::header::{HeaderMap, HeaderValue, COOKIE};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about)]
+struct Args {
+    #[arg(short, long)]
+    cookie: String,
+
+    #[arg(short, long)]
+    date: u32,
+
+    #[arg(short, long)]
+    year: Option<i32>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let args = Args::parse();
+
+    let year = args.year.unwrap_or_else(|| Utc::now().year());
+    let url = format!(
+        "https://adventofcode.com/{year}/day/{date}/input",
+        date = args.date,
+        year = year,
+    );
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        COOKIE,
+        HeaderValue::from_str(&format!("session={}", args.cookie))?,
+    );
+
+    let response = client.get(url).headers(headers).send()?;
+    if response.status().is_success() {
+        // Write into the file
+        let path = format!("./{year}/src/data/d{date}.txt", date = args.date);
+        let file_path = Path::new(&path);
+        write(file_path, response.text()?)?;
+        println!("Wrote to {}", file_path.display());
+    } else {
+        eprintln!("Request failed with status: {}", response.status());
+    }
+
+    Ok(())
+}
