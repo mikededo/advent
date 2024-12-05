5a939271b0d3cc5e34610b3f8f01a656508fa2e6
Miquel de Domingo <miquelddg@gmail.com>
2024-12-05 11:26:14 +0100

    feat(2024): 5.b solution

--- aoc-24/src/solutions/d5.rs
+++ aoc-24/src/solutions/d5.rs
@@ -31,13 +31,10 @@ pub fn solve_a() {
         }
 
         if parse_updates {
-            let values: Vec<u32> = line.split(',').map(|n| n.parse::<u32>().unwrap()).collect();
+            let values: Vec<u32> = line.split(',').filter_map(|n| n.parse().ok()).collect();
             res.push(is_rule_valid(&graph, &values));
         } else {
-            let rule = line
-                .split('|')
-                .map(|n| n.parse::<u32>().unwrap())
-                .collect::<Vec<u32>>();
+            let rule: Vec<u32> = line.split('|').filter_map(|n| n.parse().ok()).collect();
             graph
                 .entry(rule[0])
                 .and_modify(|s| {
@@ -46,6 +43,69 @@ pub fn solve_a() {
                 .or_insert(HashSet::from([rule[1]]));
         }
     });
+
+    println!("{:?}", res.iter().sum::<u32>());
+}
+
+fn can_be_first(graph: &HashMap<u32, HashSet<u32>>, value: u32, rest: &[&u32]) -> bool {
+    let empty_rules = HashSet::new();
+    let rules = graph.get(&value).unwrap_or(&empty_rules);
+    rest.iter().all(|n| rules.contains(*n))
+}
+
+fn validate_rule(graph: &HashMap<u32, HashSet<u32>>, rule: &[u32]) -> u32 {
+    let mut corrected: Vec<u32> = Vec::new();
+    let mut stack = rule.to_vec();
+    let mut i = 0;
+    while !stack.is_empty() {
+        if i == stack.len() && stack.len() > 1 {
+            i = 0;
+            continue;
+        } else if stack.len() == 1 {
+            corrected.push(stack[0]);
+            stack.pop();
+            continue;
+        }
+
+        // Check if the current number can be first
+        let rest: Vec<&u32> = stack[..i].iter().chain(&stack[i + 1..]).collect();
+        if can_be_first(graph, stack[i], &rest) {
+            corrected.push(stack[i]);
+            stack.remove(i);
+        } else {
+            i += 1;
+        }
+    }
+
+    corrected[corrected.len() / 2]
+}
+
+pub fn solve_b() {
+    let mut graph: HashMap<u32, HashSet<u32>> = HashMap::new();
+    let mut parse_updates = false;
+    let mut res = Vec::new();
+
+    for line in read_lines("d5.txt") {
+        if line.is_empty() {
+            parse_updates = true;
+            continue;
+        }
+
+        if parse_updates {
+            let values: Vec<u32> = line.split(',').filter_map(|n| n.parse().ok()).collect();
+            if is_rule_valid(&graph, &values) == 0 {
+                res.push(validate_rule(&graph, &values));
+            }
+        } else {
+            let rule: Vec<u32> = line.split('|').filter_map(|n| n.parse().ok()).collect();
+            graph
+                .entry(rule[0])
+                .and_modify(|s| {
+                    s.insert(rule[1]);
+                })
+                .or_insert(HashSet::from([rule[1]]));
+        }
+    }
 
     println!("{:?}", res.iter().sum::<u32>());
 }
