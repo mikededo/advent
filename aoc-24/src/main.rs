5ec5fc28fba0df3ccd1df0076c066431b6c2a9bd
Miquel de Domingo <miquelddg@gmail.com>
2024-12-08 09:52:27 +0100

    feat(aoc-24): 8.b solution

--- aoc-24/src/main.rs
+++ aoc-24/src/main.rs
@@ -3,5 +3,5 @@
 mod solutions;
 
 fn main() {
-    solutions::d8::solve_a();
+    solutions::d8::solve_b();
 }
--- aoc-24/src/solutions/d8.rs
+++ aoc-24/src/solutions/d8.rs
@@ -41,3 +41,50 @@ pub fn solve_a() {
 
     println!("{}", antinodes.len());
 }
+
+pub fn solve_b() {
+    let mut antennas: HashMap<char, HashSet<(i32, i32)>> = HashMap::new();
+    let map = read_chars_with("d8.txt", 24, |ch, (row, col)| {
+        if ch == '.' {
+            return;
+        }
+        let pair = (row as i32, col as i32);
+        antennas.entry(ch).or_default().insert(pair);
+    });
+
+    let map_range = (map.len() as i32, map[0].len() as i32);
+    let in_range =
+        |&(row, col): &(i32, i32)| row >= 0 && col >= 0 && row < map_range.0 && col < map_range.1;
+
+    let mut antinodes: HashSet<(i32, i32)> = HashSet::new();
+    for values in antennas.values() {
+        let iter: Vec<(i32, i32)> = values.iter().cloned().collect();
+        // All antennas with at least two of their type
+        if iter.len() > 1 {
+            antinodes.extend(&iter);
+        } else {
+            continue;
+        }
+
+        iter.iter().enumerate().for_each(|(i, curr)| {
+            iter[i + 1..].iter().for_each(|next| {
+                let delta_x = next.0 - curr.0;
+                let delta_y = next.1 - curr.1;
+
+                let mut point = (curr.0 - delta_x, curr.1 - delta_y);
+                while in_range(&point) {
+                    antinodes.insert(point);
+                    point = (point.0 - delta_x, point.1 - delta_y);
+                }
+
+                point = (next.0 + delta_x, next.1 + delta_y);
+                while in_range(&point) {
+                    antinodes.insert(point);
+                    point = (point.0 + delta_x, point.1 + delta_y);
+                }
+            });
+        });
+    }
+
+    println!("\n{}", antinodes.len());
+}
