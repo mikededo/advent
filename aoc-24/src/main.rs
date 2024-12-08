2967bb7ae7f150da62d2d4728ec3dad894fb65ff
Miquel de Domingo <miquelddg@gmail.com>
2024-12-08 09:01:38 +0100

    feat(aoc-24): 8.a solutions

--- aoc-24/src/main.rs
+++ aoc-24/src/main.rs
@@ -3,5 +3,5 @@
 mod solutions;
 
 fn main() {
-    solutions::d7::solve_b();
+    solutions::d8::solve_a();
 }
---
+++ aoc-24/src/solutions/d8.rs
@@ -0,0 +1,43 @@
+use std::collections::{HashMap, HashSet};
+
+use utils::read_chars_with;
+
+fn generate_points(p1: (i32, i32), p2: (i32, i32)) -> ((i32, i32), (i32, i32)) {
+    let dx = p2.0 - p1.0;
+    let dy = p2.1 - p1.1;
+
+    ((p1.0 - dx, p1.1 - dy), (p2.0 + dx, p2.1 + dy))
+}
+
+pub fn solve_a() {
+    let mut antennas: HashMap<char, HashSet<(i32, i32)>> = HashMap::new();
+    let map = read_chars_with("d8.txt", 24, |ch, (row, col)| {
+        if ch == '.' {
+            return;
+        }
+        let pair = (row as i32, col as i32);
+        antennas.entry(ch).or_default().insert(pair);
+    });
+
+    let map_range = (map.len() as i32, map[0].len() as i32);
+    let in_range =
+        |&(row, col): &(i32, i32)| row >= 0 && col >= 0 && row < map_range.0 && col < map_range.1;
+
+    let mut antinodes: HashSet<(i32, i32)> = HashSet::new();
+    for values in antennas.values() {
+        let iter: Vec<(i32, i32)> = values.iter().cloned().collect();
+        iter.iter().enumerate().for_each(|(i, curr)| {
+            iter[i + 1..].iter().for_each(|next| {
+                let (p1, p2) = generate_points(*curr, *next);
+                if in_range(&p1) {
+                    antinodes.insert(p1);
+                }
+                if in_range(&p2) {
+                    antinodes.insert(p2);
+                }
+            });
+        });
+    }
+
+    println!("{}", antinodes.len());
+}
