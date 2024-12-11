1d1de5686e620c879ae281da840e8231b143d015
Miquel de Domingo <miquelddg@gmail.com>
2024-12-11 10:10:18 +0100

    feat(aoc-24): 11.b solution
    Basically use a cache for each number and blinks missing. This can be
    done because the pattern is not random, so at some point the numbers
    repeat itself.

--- aoc-24/src/main.rs
+++ aoc-24/src/main.rs
@@ -3,5 +3,5 @@
 mod solutions;
 
 fn main() {
-    solutions::d11::solve_a();
+    solutions::d11::solve_b();
 }
--- aoc-24/src/solutions/d11.rs
+++ aoc-24/src/solutions/d11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use utils::read_lines;
 
 fn count_digits(mut n: u64) -> u32 {
@@ -34,6 +36,42 @@ fn blink(v: Vec<u64>) -> Vec<u64> {
     result
 }
 
+struct StoneCounter {
+    cache: HashMap<(u64, u64), u64>,
+}
+
+impl StoneCounter {
+    fn new() -> StoneCounter {
+        StoneCounter {
+            cache: HashMap::new(),
+        }
+    }
+
+    fn count(&mut self, n: u64, blinks: u64) -> u64 {
+        if blinks == 0 {
+            return 1;
+        }
+
+        if let Some(c) = self.cache.get(&(n, blinks)) {
+            return *c;
+        }
+
+        let mut count = 0;
+        let digits = count_digits(n);
+        if n == 0 {
+            count += self.count(1, blinks - 1);
+        } else if digits % 2 == 0 {
+            let exp = 10u64.pow(digits / 2);
+            count += self.count(n / exp, blinks - 1) + self.count(n % exp, blinks - 1);
+        } else {
+            count += self.count(n * 2024, blinks - 1);
+        }
+
+        self.cache.insert((n, blinks), count);
+        count
+    }
+}
+
 pub fn solve_a() {
     let mut res = read_lines("d11.txt", 24)
         .into_iter()
@@ -48,4 +86,20 @@ pub fn solve_a() {
     }
 
     println!("{:?}", res.len());
+}
+
+pub fn solve_b() {
+    let mut counter = StoneCounter::new();
+    let res = read_lines("d11.txt", 24)
+        .into_iter()
+        .next()
+        .unwrap_or_else(|| unreachable!())
+        .split_whitespace()
+        .map(|s| match s.parse::<u64>() {
+            Ok(n) => counter.count(n, 75),
+            Err(_) => 0,
+        })
+        .sum::<u64>();
+
+    println!("{:?}", res);
 }
