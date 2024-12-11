8ffde460289e7d36c35f2efc3c7714f275edf1d2
Miquel de Domingo <miquelddg@gmail.com>
2024-12-11 10:14:19 +0100

    refactor(aoc-24): 11 reuse the class

--- aoc-24/src/solutions/d11.rs
+++ aoc-24/src/solutions/d11.rs
@@ -2,40 +2,6 @@ use std::collections::HashMap;
 
 use utils::read_lines;
 
-fn count_digits(mut n: u64) -> u32 {
-    if n == 0 {
-        return 1;
-    }
-    let mut digits = 0;
-    while n > 0 {
-        n /= 10;
-        digits += 1;
-    }
-    digits
-}
-
-fn blink(v: Vec<u64>) -> Vec<u64> {
-    let mut result = Vec::new();
-
-    for n in v.iter() {
-        match n {
-            0 => result.push(1),
-            _ => {
-                let digits = count_digits(*n);
-                if digits % 2 == 0 {
-                    let exp = 10u64.pow(digits / 2);
-                    result.push(n / exp);
-                    result.push(n % exp);
-                } else {
-                    result.push(n * 2024);
-                }
-            }
-        }
-    }
-
-    result
-}
-
 struct StoneCounter {
     cache: HashMap<(u64, u64), u64>,
 }
@@ -57,7 +23,7 @@ impl StoneCounter {
         }
 
         let mut count = 0;
-        let digits = count_digits(n);
+        let digits = StoneCounter::count_digits(n);
         if n == 0 {
             count += self.count(1, blinks - 1);
         } else if digits % 2 == 0 {
@@ -70,36 +36,48 @@ impl StoneCounter {
         self.cache.insert((n, blinks), count);
         count
     }
+
+    fn count_digits(mut n: u64) -> u32 {
+        if n == 0 {
+            return 1;
+        }
+        let mut digits = 0;
+        while n > 0 {
+            n /= 10;
+            digits += 1;
+        }
+        digits
+    }
 }
 
 pub fn solve_a() {
-    let mut res = read_lines("d11.txt", 24)
+    let mut counter = StoneCounter::new();
+    let res: u64 = read_lines("d11.txt", 24)
         .into_iter()
         .next()
         .unwrap_or_else(|| unreachable!())
         .split_whitespace()
-        .filter_map(|s| s.parse().ok())
-        .collect::<Vec<u64>>();
+        .map(|s| match s.parse() {
+            Ok(n) => counter.count(n, 25),
+            Err(_) => 0,
+        })
+        .sum();
 
-    for _ in 0..25 {
-        res = blink(res);
-    }
-
-    println!("{:?}", res.len());
+    println!("{res}");
 }
 
 pub fn solve_b() {
     let mut counter = StoneCounter::new();
-    let res = read_lines("d11.txt", 24)
+    let res: u64 = read_lines("d11.txt", 24)
         .into_iter()
         .next()
         .unwrap_or_else(|| unreachable!())
         .split_whitespace()
-        .map(|s| match s.parse::<u64>() {
+        .map(|s| match s.parse() {
             Ok(n) => counter.count(n, 75),
             Err(_) => 0,
         })
-        .sum::<u64>();
+        .sum();
 
-    println!("{:?}", res);
+    println!("{res}");
 }
