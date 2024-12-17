adfb4fe48af968e15dd9220539b3c58229fcdc3f
Miquel de Domingo <miquelddg@gmail.com>
2024-12-17 15:03:01 +0100

    feat(aoc-24): 16.b solution

--- aoc-24/src/solutions/d16.rs
+++ aoc-24/src/solutions/d16.rs
@@ -1,6 +1,6 @@
 use std::{
     cmp::Reverse,
-    collections::{BinaryHeap, HashMap},
+    collections::{BinaryHeap, HashMap, HashSet},
 };
 
 use utils::read_lines;
@@ -15,6 +15,10 @@ enum Direction {
 
 impl Direction {
     pub const ALL: [Self; 4] = [Self::Up, Self::Right, Self::Down, Self::Left];
+
+    fn is_vertical(&self) -> bool {
+        *self == Self::Up || *self == Self::Down
+    }
 }
 
 fn next_direction((x, y): (usize, usize), direction: Direction) -> (usize, usize) {
@@ -54,8 +58,8 @@ impl Map {
         self.map[row].push(c);
     }
 
-    // I always have a hard time implementing Djiktra...
-    fn find_path(&self, start: Node, end: Node) -> usize {
+    // I always have a hard time implementing Djikstra...
+    fn find_min_cost(&self, start: Node, end: Node) -> usize {
         let mut dist = HashMap::new();
         let mut heap = BinaryHeap::new();
         let mut min_cost = usize::MAX;
@@ -94,6 +98,53 @@ impl Map {
 
         min_cost
     }
+
+    fn find_all_min(&self, start: Node, end: Node) -> usize {
+        let min_cost = self.find_min_cost(start, end);
+        let mut dist = HashMap::new();
+        let mut heap = BinaryHeap::new();
+        // We will store all the points that are contained in one of the paths that arrive to the
+        // end with the minimum cost
+        let mut points = HashSet::new();
+
+        dist.insert(start, [min_cost, min_cost]);
+        // (cost, (node, direction, visited))
+        heap.push(Reverse((0usize, (start, Direction::Right, vec![]))));
+
+        while let Some(Reverse((cost, (node, from, mut visited)))) = heap.pop() {
+            let axis = from.is_vertical() as usize;
+            visited.push(node);
+
+            // The cost is already higher than the minimum found
+            if cost > dist[&node][axis] || cost > min_cost {
+                continue;
+            }
+
+            if node == end {
+                if cost == min_cost {
+                    points.extend(visited);
+                }
+                continue;
+            }
+
+            for to in Direction::ALL {
+                let next = next_direction(node, to);
+                let cost = cost + if from == to { 1 } else { 1001 };
+
+                if self.map[next.0][next.1] == '#' {
+                    continue;
+                }
+
+                let entry = dist.entry(next).or_insert([min_cost, min_cost]);
+                if cost <= entry[axis] {
+                    dist.entry(next).and_modify(|e| e[axis] = cost);
+                    heap.push(Reverse((cost, (next, to, visited.clone()))));
+                }
+            }
+        }
+
+        points.len()
+    }
 }
 
 pub fn solve_a() {
@@ -114,5 +165,26 @@ pub fn solve_a() {
         })
     });
 
-    println!("{:?}", g.find_path(start, end));
+    println!("{:?}", g.find_min_cost(start, end));
+}
+
+pub fn solve_b() {
+    let mut g = Map::new();
+    let mut start = (0, 0);
+    let mut end = (0, 0);
+
+    let map = read_lines("d16.txt", 24);
+    map.iter().enumerate().for_each(|(i, row)| {
+        row.chars().enumerate().for_each(|(j, c)| {
+            match c {
+                'S' => start = (i, j),
+                'E' => end = (i, j),
+                _ => (),
+            }
+
+            g.map_char(i, c);
+        })
+    });
+
+    println!("{:?}", g.find_all_min(start, end));
 }
