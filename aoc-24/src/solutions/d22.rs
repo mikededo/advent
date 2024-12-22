afaca54b647d103f91fb489bc8198e78f3a6d907
Miquel de Domingo <miquelddg@gmail.com>
2024-12-22 09:28:38 +0100

    feat(aoc-24): 22.b solution

--- aoc-24/src/solutions/d22.rs
+++ aoc-24/src/solutions/d22.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
 use utils::read_lines;
 
 fn next(n: isize) -> isize {
@@ -25,4 +28,34 @@ pub fn solve_a() {
         .sum::<isize>();
 
     println!("{res}");
+}
+
+pub fn solve_b() {
+    let res = read_lines("d22.txt", 24)
+        .iter()
+        .map(|l| {
+            let mut n = l.parse::<isize>().unwrap();
+            let mut res = Vec::new();
+
+            (0..2000).for_each(|_| {
+                res.push(n % 10);
+                n = next(n);
+            });
+
+            res.into_iter()
+                .rev()
+                .tuple_windows::<(_, _, _, _, _)>()
+                .map(|(a, b, c, d, e)| ((d - e, c - d, b - c, a - b), a))
+                .collect::<HashMap<_, _>>()
+        })
+        .fold(HashMap::new(), |mut acc, seq| {
+            seq.into_iter()
+                .for_each(|(k, v)| *acc.entry(k).or_insert(0) += v);
+            acc
+        })
+        .into_values()
+        .max()
+        .unwrap();
+
+    println!("{res:?}");
 }
